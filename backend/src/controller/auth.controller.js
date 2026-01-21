2c74c145aa29b381d199ee4c47f02330e2e9770a
Ayush Kumar <aayushkumar1228@gmail.com>
2026-01-22 00:54:01 +0530

    redirect to auth when token is missing

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -4,7 +4,7 @@ import wrapAsync from "../utils/tryCatchWrapper.js";
 
 export const register_user = wrapAsync(async (req, res) => {
     const { name, email, password } = req.body;
-    const {token, user} = await registerUser(name, email, password);
+    const { token, user } = await registerUser(name, email, password);
     req.user = user
     res.cookie('accessToken', token, cookieOptions);
     res.status(200).json({ message: 'User registered successfully' });
@@ -17,3 +17,12 @@ export const login_user = wrapAsync(async (req, res) => {
     res.cookie('accessToken', token, cookieOptions);
     res.status(200).json({ user: user, message: 'Login successfully' });
 });
+
+export const logout_user = wrapAsync(async (req, res) => {
+    res.clearCookie("accessToken", cookieOptions)
+    res.status(200).json({ message: "logout success" })
+});
+
+export const get_current_user = wrapAsync( async (req, res) => {
+    res.status(200).json({user:req.user})
+})
