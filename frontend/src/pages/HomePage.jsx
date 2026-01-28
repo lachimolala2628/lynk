ea4266a6e9548dfbc13d43f678333ee7ff03c9b8
Ayush Kumar <aayushkumar1228@gmail.com>
2026-01-29 05:20:31 +0530

    spelling fixes

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -6,7 +6,7 @@ const HomePage = () => {
         <div className='min-h-screen bg-[#0A0A0A] flex flex-col items-center justify-center p-4'>
             <div className='border border-[#222323] p-8 rounded-lg shadow-md w-full max-w-md'>
                 <h1 className='text-2xl font-mono text-center mb-6 text-[#FFFFFF]'>
-                    URL Shortner
+                    URL Shortener
                 </h1>
                 <UrlForm />
             </div>
